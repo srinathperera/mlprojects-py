df6b50280b0cfdc2bd3e71124392fda00d57acaf
Srinath Perera <srinath@Srinaths-MacBook-Pro.local>
2016-05-17 09:30:44 +0530

    updates

--- TimeSeriesRegression/DLRegressionOnTrasformed.py
+++ TimeSeriesRegression/DLRegressionOnTrasformed.py
@@ -8,10 +8,10 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 
-import matplotlib.pylab as plt
-import seaborn as sns
-sns.set_style("darkgrid")
-sns.set_context("poster")
+#import matplotlib.pylab as plt
+#import seaborn as sns
+#sns.set_style("darkgrid")
+#sns.set_context("poster")
 
 
 from sklearn.linear_model import LinearRegression
