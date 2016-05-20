fdb21e4b58bce6fc66ecd080588b7141b9900623
srinathperera <hemapani@gmail.com>
2016-05-20 07:15:37 +0530

    Commit message

--- TimeSeriesRegression/DLRegressionOnTrasformed.py
+++ TimeSeriesRegression/DLRegressionOnTrasformed.py
@@ -126,9 +126,9 @@ configs = [
 #    MLConfigs(nodes_in_layer = 500, number_of_hidden_layers = 2, droput = 0, activation_fn='relu', loss= "mse",
 #        epoch_count = 10, optimizer = SGD(lr=0.1, decay=1e-3, momentum=0.99, nesterov=True)),
     MLConfigs(nodes_in_layer = 1000, number_of_hidden_layers = 2, droput = 3, activation_fn='relu', loss= "mse",
-        epoch_count = 20, optimizer = SGD(lr=0.1, decay=0.0005, momentum=0.99, nesterov=True)),
+        epoch_count = 20, optimizer = Adam()),
     MLConfigs(nodes_in_layer = 1000, number_of_hidden_layers = 2, droput = 2, activation_fn='relu', loss= "mse",
-        epoch_count = 20, optimizer = SGD(lr=0.1, decay=0.0005, momentum=0.99, nesterov=True)),
+        epoch_count = 20, optimizer = SGD(lr=0.01, decay=0, momentum=0.9, nesterov=True)),
 
     #MLConfigs(nodes_in_layer = 500, number_of_hidden_layers = 2, droput = 0, activation_fn='relu', loss= "mse",
     #    epoch_count = 10, optimizer = SGD(lr=0.1, decay=1e-3, momentum=0.99, nesterov=True)),
