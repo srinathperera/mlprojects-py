f2165e71e2c0a24999015579d0b28dea8eadd0e9
Srinath Perera <hemapani@gmail.com>
2016-08-14 15:47:19 +0530

    Commit message

--- TimeSeriesRegression/inventory_demand/__init__.py
+++ TimeSeriesRegression/inventory_demand/__init__.py
@@ -1146,7 +1146,7 @@ def merge_another_dataset(train_df, test_df, sub_df, analysis_type, cmd, feilds_
     merge_feilds = ['Agencia_ID' , 'Canal_ID', 'Ruta_SAK', 'Cliente_ID', 'Producto_ID']
     feilds_to_use = feilds_to_use + merge_feilds
 
-    sup_train_df, sup_test_df, sup_sub_df, _, _ = load_train_data(analysis_type, cmd)
+    sup_train_df, sup_test_df, sup_sub_df, _, _ = load_train_data(analysis_type, cmd, throw_error=True)
     sup_train_df = sup_train_df[feilds_to_use]
     sup_test_df = sup_test_df[feilds_to_use]
     sup_sub_df = sup_sub_df[feilds_to_use]
