138dd796653a9adc8fec41652ce52425f441abe6
Srinath Perera <hemapani@gmail.com>
2016-08-20 14:01:06 +0530

    Commit message

--- TimeSeriesRegression/ParamExploreForecast.py
+++ TimeSeriesRegression/ParamExploreForecast.py
@@ -60,7 +60,7 @@ if feature_set is None or feature_set == "feature-explore":
     ]
     '''
 
-    top_group = [
+    top_group2 = [
         ['clients_combined_dp_Mean', 'clients_combined_dpci', 'clients_combined_dp_median'],
         ['clients_combined_vh_Mean_x', 'clients_combined_vhci_x', 'clients_combined_vh_median_x'],
         ['Agencia_ID_Demanda_uni_equil_Mean', 'Agencia_ID_Demanda_uni_equilci', 'Agencia_ID_Demanda_uni_equil_median'],
@@ -69,7 +69,7 @@ if feature_set is None or feature_set == "feature-explore":
     ]
 
 
-    groups = [
+    groups2 = [
         ['clients_combined_dp_Mean', 'clients_combined_dpci', 'clients_combined_dp_median'],
         ['clients_combined_vh_Mean_x', 'clients_combined_vhci_x', 'clients_combined_vh_median_x'],
         ['Agencia_ID_Demanda_uni_equil_Mean', 'Agencia_ID_Demanda_uni_equilci', 'Agencia_ID_Demanda_uni_equil_median'],
@@ -100,23 +100,22 @@ if feature_set is None or feature_set == "feature-explore":
         ["entropy", "corr", "mean_autocorr", "mean_corss_points_count"]
     ]
 
-    group1 = [
-        ['Semana', 'Agencia_ID', 'Canal_ID', 'Ruta_SAK', 'Cliente_ID', 'Producto_ID'],
-        ['Agencia_ID_Demanda_uni_equil_Mean', 'Agencia_ID_Demanda_uni_equilci', 'Agencia_ID_Demanda_uni_equil_median'],
-        ['Agencia_ID_Dev_proxima_Mean', 'Agencia_ID_Dev_proximaci', 'Agencia_ID_Dev_proxima_median'],
-        ['Agencia_ID_Venta_hoy_Mean', 'Agencia_ID_Venta_hoyci', 'Agencia_ID_Venta_hoy_median'],
-        ['clients_combined_Mean', 'clients_combined_kurtosis', 'clients_combinedci', 'clients_combined_median'],
-        ['clients_combined_vh_Mean', 'clients_combined_vhci', 'clients_combined_vh_median'],
-        ['clients_combined_dp_Mean', 'clients_combined_dpci', 'clients_combined_dp_median'],
-        ['client_nn_Mean', 'client_nnci', 'client_nn_median'],
-        ['client_nn_vh_Mean', 'client_nn_vhci', 'client_nn_vh_median'],
-        ['client_nn_dp_Mean', 'client_nn_dpci', 'client_nn_dp_median'],
-        ['client_nn_agency_Mean', 'client_nn_agencyci', 'client_nn_agency_median'],
-        ['client_nn_agency_vh_Mean', 'client_nn_agency_vhci', 'client_nn_agency_vh_median'],
-        ['client_nn_agency_dp_Mean', 'client_nn_agency_dpci', 'client_nn_agency_dp_median'],
-        ['Producto_ID_Demanda_uni_equil_Mean', 'Producto_ID_Demanda_uni_equilci', 'Producto_ID_Demanda_uni_equil_median'],
-        ['Producto_ID_Venta_hoy_Mean', 'Producto_ID_Venta_hoyci', 'Producto_ID_Venta_hoy_median'],
-        ['Producto_ID_Dev_proxima_Mean', 'Producto_ID_Dev_proximaci', 'Producto_ID_Dev_proxima_median'],
+    groups = [
+        ['Agencia_ID_Demanda_uni_equil_Mean', 'Agencia_ID_Demanda_uni_equilci', 'Agencia_ID_Demanda_uni_equil_median']
+            + ['Agencia_ID_Dev_proxima_Mean', 'Agencia_ID_Dev_proximaci', 'Agencia_ID_Dev_proxima_median']
+            + ['Agencia_ID_Venta_hoy_Mean', 'Agencia_ID_Venta_hoyci', 'Agencia_ID_Venta_hoy_median'],
+        ['clients_combined_Mean', 'clients_combined_kurtosis', 'clients_combinedci', 'clients_combined_median']
+            + ['clients_combined_vh_Mean', 'clients_combined_vhci', 'clients_combined_vh_median']
+            + ['clients_combined_dp_Mean', 'clients_combined_dpci', 'clients_combined_dp_median'],
+        ['client_nn_Mean', 'client_nnci', 'client_nn_median']
+            + ['client_nn_vh_Mean', 'client_nn_vhci', 'client_nn_vh_median']
+            + ['client_nn_dp_Mean', 'client_nn_dpci', 'client_nn_dp_median'],
+        ['client_nn_agency_Mean', 'client_nn_agencyci', 'client_nn_agency_median']
+            + ['client_nn_agency_vh_Mean', 'client_nn_agency_vhci', 'client_nn_agency_vh_median']
+            + ['client_nn_agency_dp_Mean', 'client_nn_agency_dpci', 'client_nn_agency_dp_median'],
+        ['Producto_ID_Demanda_uni_equil_Mean', 'Producto_ID_Demanda_uni_equilci', 'Producto_ID_Demanda_uni_equil_median']
+            + ['Producto_ID_Venta_hoy_Mean', 'Producto_ID_Venta_hoyci', 'Producto_ID_Venta_hoy_median']
+            + ['Producto_ID_Dev_proxima_Mean', 'Producto_ID_Dev_proximaci', 'Producto_ID_Dev_proxima_median'],
         ['weight', 'pieces', 'has_choco', 'has_vanilla', 'has_multigrain'],
         ['brand_id_Demanda_uni_equil_Mean', 'brand_id_Demanda_uni_equilci', 'brand_id_Demanda_uni_equil_median','product_word_Demanda_uni_equil_Mean', 'product_word_Demanda_uni_equilci', 'product_word_Demanda_uni_equil_median'],
         ['Town_id_Demanda_uni_equil_Mean', 'Town_id_Demanda_uni_equilci', 'Town_id_Demanda_uni_equil_median', 'State_id_Demanda_uni_equil_Mean', 'State_id_Demanda_uni_equilci', 'State_id_Demanda_uni_equil_median'],
@@ -128,9 +127,11 @@ if feature_set is None or feature_set == "feature-explore":
 
     features = []
     for t in list(itertools.combinations(range(len(groups)), 4)):
-        flist = top_group[random.randint(0, len(top_group)-1)] + groups[t[0]] + groups[t[1]] + groups[t[2]] + groups[t[3]]
-        fset = list(set(flist))
-        features.append(fset)
+        #flist = top_group[random.randint(0, len(top_group)-1)] + groups[t[0]] + groups[t[1]] + groups[t[2]] + groups[t[3]]
+        #fset = list(set(flist))
+        #features.append(fset)
+        flist = groups[t[0]] + groups[t[1]] + groups[t[2]]
+        features.append(flist)
 
     np.random.shuffle(features)
     features = features[:200]
@@ -140,7 +141,7 @@ else:
         flist = ['Producto_ID_Demanda_uni_equil_Mean', 'Producto_ID_Demanda_uni_equilci', 'Producto_ID_Demanda_uni_equil_median', 'Producto_ID_Venta_hoy_Mean', 'Producto_ID_Venta_hoyci', 'Producto_ID_Venta_hoy_median', 'clients_combined_vh_Mean', 'clients_combined_vhci', 'clients_combined_vh_median', 'mean_sales', 'sales_count', 'sales_stddev', 'median_sales', 'hmean', 'entropy']
     elif feature_set == "vh-mean-product":
         flist = ['clients_combined_vh_Mean', 'clients_combined_vhci', 'clients_combined_vh_median', 'Producto_ID_Dev_proxima_Mean', 'Producto_ID_Dev_proximaci', 'Producto_ID_Dev_proxima_median', 'weight', 'pieces', 'signature']
-    elif feature_set == "nn_features":
+    elif feature_set == "nn_features": #0.5737
         flist = ['client_nn_Mean', 'client_nnci', 'client_nn_median']\
             + ['client_nn_vh_Mean', 'client_nn_vhci', 'client_nn_vh_median']\
             + ['client_nn_dp_Mean', 'client_nn_dpci', 'client_nn_dp_median']\
